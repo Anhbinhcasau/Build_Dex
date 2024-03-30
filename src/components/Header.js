b53d9739eee0da1920b2d9b7c8349048d7e1e649
Anhbinhcasau <huynhthanhbinhbl2003@gmail.com>
2024-03-31 00:46:50 +0700

    UI code

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Logo from "../moralis-logo.svg";
+import eth  from "../eth.svg";
+
+function Header() {
+  return (
+    <header>
+      <div className='leftH'>
+        <img src={Logo} alt='logo' className='logo'  />
+        <div className = 'headerItem'> Swap </div>
+        <div className = 'headerItem'> Tokens </div>
+      </div>
+      <div className='rightH'>
+        <div className='headerItem'>
+          <img src={eth} alt='eth' className='eth'/>
+          Ethereum
+        </div>
+        <div className='connectButton'>
+          Connect   
+        </div>
+      </div>
+      
+    </header>
+  )
+}
+
+export default Header
