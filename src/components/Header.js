0454c7854db2d1326dd7b91674cc9390c91f6aec
Anhbinhcasau <huynhthanhbinhbl2003@gmail.com>
2024-03-31 22:27:49 +0700

    Build Swap Page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,30 @@
-import React from 'react'
+import React from "react";
 import Logo from "../moralis-logo.svg";
-import eth  from "../eth.svg";
+import eth from "../eth.svg";
+import {  Link } from "react-router-dom";
 
 function Header() {
   return (
     <header>
-      <div className='leftH'>
-        <img src={Logo} alt='logo' className='logo'  />
-        <div className = 'headerItem'> Swap </div>
-        <div className = 'headerItem'> Tokens </div>
+      <div className="leftH">
+        <img src={Logo} alt="logo" className="logo" />
+        <Link to="/" className="link">
+          <div className="headerItem"> Swap </div>
+        </Link>
+        <Link to="/tokens" className="link" >
+          <div className="headerItem"> Tokens </div>
+        </Link>
       </div>
-      <div className='rightH'>
-        <div className='headerItem'>
-          <img src={eth} alt='eth' className='eth'/>
+      <div className="rightH">
+        <div className="headerItem">
+          <img src={eth} alt="eth" className="eth"/>
           Ethereum
         </div>
-        <div className='connectButton'>
-          Connect   
+        <div className="connectButton">
+          Connect Wallet
         </div>
       </div>
-      
     </header>
-  )
+  );
 }
-
-export default Header
+export default Header;
